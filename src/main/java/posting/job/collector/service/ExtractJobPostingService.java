c9daea8bba06f7c367bf756638568167c4eca492
jsoyun <syusmm233@gmail.com>
2025-01-27 01:58:44 +0900

    feature(TargetSource) : TargetSource 회사 추가

--- src/main/java/posting/job/collector/service/ExtractJobPostingService.java
+++ src/main/java/posting/job/collector/service/ExtractJobPostingService.java
@@ -2,17 +2,28 @@ package posting.job.collector.service;
 
 import org.springframework.stereotype.Service;
 import posting.job.collector.configuration.TargetSource;
-import posting.job.collector.service.extractor.KakaoJobPostingExtractor;
-import posting.job.collector.service.extractor.NaverJobPostingExtractor;
+import posting.job.collector.service.extractor.*;
 
 @Service
 public class ExtractJobPostingService {
+    private final UrlDecoder urlDecoder;  // UrlDecoder를 주입받기 위해 필드 추가
+
+    public ExtractJobPostingService(UrlDecoder urlDecoder) {
+        this.urlDecoder = urlDecoder;
+    }
     public String execute(TargetSource item) throws Exception {
         return switch (item) {
             case NAVER -> new NaverJobPostingExtractor(item.getUrl()).extract();
-            case NHN -> "NHN";
-            case KAKAO -> new KakaoJobPostingExtractor(item.getUrl()).extract();
-            case COUPANG -> "COUPANG";
+            case NHN ->  new NhnJobPostingExtractor(item.getUrl()).extract();
+            case KAKAO -> new KakaoJobPostingExtractor(item.getUrl(), urlDecoder).extract();
+            case COUPANG -> new CoupangPostingExtractor(item.getUrl()).extract();
+            case WOOWAHAN -> new WoowahanJobPostingExtractor(item.getUrl()).extract();
+            case SOCARCORP -> new SocarCorpJobPostingExtractor(item.getUrl()).extract();
+            case TOSS -> new TossJobPostingExtractor(item.getUrl()).extract();
+            case DUNAMU -> new DunamuJobPostingExtractor(item.getUrl()).extract();
+            case HYUNDAI -> new HyundaiJobPostingExtractor(item.getUrl()).extract();
+            case SAMSUNG -> new SamsungJobPostingExtractor(item.getUrl()).extract();
+
         };
     }
 }
