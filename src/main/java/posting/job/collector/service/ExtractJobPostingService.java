593d2c4cffdddc906a0755b2fbb5b2cef6debaa4
jsoyun <syusmm233@gmail.com>
2025-02-23 16:04:24 +0900

    fix(SamsungJobPosting-): 삼성, 쿠팡 공고 크롤러 실행 임시 제거

--- src/main/java/posting/job/collector/service/ExtractJobPostingService.java
+++ src/main/java/posting/job/collector/service/ExtractJobPostingService.java
@@ -7,18 +7,17 @@ import posting.job.collector.service.extractor.*;
 @Service
 public class ExtractJobPostingService {
 
+
     public String execute(TargetSource item) throws Exception {
         return switch (item) {
             case NAVER -> new NaverJobPostingExtractor(item.getUrl()).extract();
             case NHN ->  new NhnJobPostingExtractor(item.getUrl()).extract();
             case KAKAO -> new KakaoJobPostingExtractor(item.getUrl()).extract();
-            case COUPANG -> new CoupangPostingExtractor(item.getUrl()).extract();
             case WOOWAHAN -> new WoowahanJobPostingExtractor(item.getUrl()).extract();
             case SOCARCORP -> new SocarCorpJobPostingExtractor(item.getUrl()).extract();
             case TOSS -> new TossJobPostingExtractor(item.getUrl()).extract();
             case DUNAMU -> new DunamuJobPostingExtractor(item.getUrl()).extract();
             case HYUNDAI -> new HyundaiJobPostingExtractor(item.getUrl()).extract();
-            case SAMSUNG -> new SamsungJobPostingExtractor(item.getUrl()).extract();
 
         };
     }
